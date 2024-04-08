433048d7c4ff6caa96c658b0bcfa00202a2d2109
Smit <smitdev2553@gmail.com>
2024-04-07 23:50:14 -0700

    Updated metadata and layout for personal website

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,19 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div>
-        <h3>Presenting,</h3>
+        <h2>Presenting,</h2>
         <h1>Smit Devrukhkar</h1>
-        <p>Developer</p>
+        <h2>Developer</h2>
+        <div className={styles.container1}>
+          <a href="/projects" className={styles.link}>
+            Projects
+          </a>
+          <a href="/resume" className={styles.link}>
+            Resume
+          </a>
+        </div>
       </div>
+
       <div className={styles.imageContainer}>
         <Image
           className={styles.profilepicture}
