b0a0734073aa48fc5138559ca4c0e27776877db1
Smit <smitdev2553@gmail.com>
2024-08-21 17:31:42 -0700

    style: Remove unused resumeItem component and its styles

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
-              Resume
+              Experience
             </motion.a>
           </div>
           <div className={styles.container2}>
